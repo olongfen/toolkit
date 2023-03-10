e8d738fec538795b50237744b1f4760398cc685c
olongfen <824426699@qq.com>
2023-03-10 14:38:21 +0800

    chore: 修改response

--- multi/xerror/error_map.go
+++ multi/xerror/error_map.go
@@ -11,19 +11,25 @@ var (
 	}
 )
 
+const (
+	SimplifiedChinese  = "zh-cn"
+	TraditionalChinese = "zh-tw"
+	English            = "en"
+)
+
 func init() {
-	DefaultErrorMul.Set(IllegalAccessToken, "zh-cn", "非法token")
-	DefaultErrorMul.Set(IllegalAccessToken, "en", "Illegal token")
-	DefaultErrorMul.Set(IllegalCertificate, "zh-cn", "非法凭证")
-	DefaultErrorMul.Set(IllegalCertificate, "en", "Illegal certificate")
-	DefaultErrorMul.Set(IllegalParameter, "zh-cn", "非法参数")
-	DefaultErrorMul.Set(IllegalParameter, "en", "Illegal parameter")
-	DefaultErrorMul.Set(RecordNotFound, "zh-cn", "记录未找到")
-	DefaultErrorMul.Set(RecordNotFound, "en", "record not found")
-	DefaultErrorMul.Set(AlreadyExists, "zh-cn", "已经存在")
-	DefaultErrorMul.Set(AlreadyExists, "en", "already exists")
-	DefaultErrorMul.Set(SortParameterMismatch, "zh-cn", "排序参数不匹配")
-	DefaultErrorMul.Set(SortParameterMismatch, "en", "sort parameter mismatch")
+	DefaultErrorMul.Set(IllegalAccessToken, SimplifiedChinese, "非法token")
+	DefaultErrorMul.Set(IllegalAccessToken, SimplifiedChinese, "Illegal token")
+	DefaultErrorMul.Set(IllegalCertificate, SimplifiedChinese, "非法凭证")
+	DefaultErrorMul.Set(IllegalCertificate, SimplifiedChinese, "Illegal certificate")
+	DefaultErrorMul.Set(IllegalParameter, SimplifiedChinese, "非法参数")
+	DefaultErrorMul.Set(IllegalParameter, SimplifiedChinese, "Illegal parameter")
+	DefaultErrorMul.Set(RecordNotFound, SimplifiedChinese, "记录未找到")
+	DefaultErrorMul.Set(RecordNotFound, SimplifiedChinese, "record not found")
+	DefaultErrorMul.Set(AlreadyExists, SimplifiedChinese, "已经存在")
+	DefaultErrorMul.Set(AlreadyExists, SimplifiedChinese, "already exists")
+	DefaultErrorMul.Set(SortParameterMismatch, SimplifiedChinese, "排序参数不匹配")
+	DefaultErrorMul.Set(SortParameterMismatch, SimplifiedChinese, "sort parameter mismatch")
 
 }
 
