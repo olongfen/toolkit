2616bf4db55a2825b89af416c561ed9e6d73c2ba
olongfen <824426699@qq.com>
2023-03-15 10:25:54 +0800

    fix: column unique

--- tools/model.go
+++ tools/model.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Model struct {
-	ID        uint           `gorm:"primarykey"`
-	Uuid      string         `gorm:"size:36;uniqueIndex;comment:唯一uuid"`
+	ID        uint           `gorm:"primarykey;autoIncrement"`
+	Uuid      string         `gorm:"size:36;unique;index;comment:唯一uuid"`
 	CreatedAt time.Time      `gorm:"comment:创建时间"`
 	UpdatedAt time.Time      `gorm:"comment:更新时间"`
 	DeletedAt gorm.DeletedAt `gorm:"index;comment:删除时间"`
